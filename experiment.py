3bc63c09bafbdec96ce4acdcafae90704661af01
Tim Day <timday@bottlenose.demon.co.uk>
2020-02-06 18:45:18 +0000

    Needs a more recent sympy?

--- experiment.py
+++ experiment.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from sympy.abc import u,v,w,t
 from sympy import Function, dsolve, Eq, Derivative, symbols
 
-y=Function('y')
 z=Function('z')
+y=Function('y')
+u,v,w,t=symbols('u,v,w,t',real=True)
 
-eq=(Eq(Derivative(z(t),t),v*y(t)-u*z(t)),Eq(Derivative(y(t),t),-v*y(t)+w*z(t)))
+eq=( Eq(Derivative(z(t),t),v*y(t)-u*z(t)) , Eq(Derivative(y(t),t),w*z(t)-v*y(t)) )
 dsolve(eq)
