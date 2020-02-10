86d95c6b63e0ad5e87efe58068b98c11ed836242
timday <timday@bottlenose.demon.co.uk>
2020-02-10 22:58:37 +0000

    Play with multi-variable

--- experiment2.py
+++ experiment2.py
@@ -14,9 +14,12 @@ import matplotlib.pyplot as plt
 #  2: Number recovering = number becoming contagious 7 days ago (contagious time)
 #  3: Number observed = Proportion of becoming contagious
 
+w=np.array([min(0.5+i,6.5-i) for i in range(7)])
+w=w/np.sum(w)
+
 def model(Y,t):
-    fresh=2.0*Y(t-7)[0]
-    recov=Y(t-7)[1]
+    fresh=1.2*np.sum(w*np.array([Y(t-7-d)[0] for d in range(7)]))  # Fresh contagious cases based on contagious cases 7-13 days ago
+    recov=np.sum(w*np.array([Y(t-7-d)[1] for d in range(7)]))      # Recovering cases based on fresh cases 7-13 days ago.
     return np.array([
         Y(t)[0]+fresh-recov,
         fresh,
@@ -25,7 +28,7 @@ def model(Y,t):
     ])
 
 def values_before_zero(t):
-    return np.array([1.0,1.0,1.0,0.0])
+    return np.array([1.0,0.0,0.0,0.0])
 
 ts=np.arange(60)
 
@@ -36,4 +39,5 @@ plt.plot(ts,ys[:,1],color='black')
 plt.plot(ts,ys[:,2],color='blue')
 plt.plot(ts,ys[:,3],color='red')
 plt.yscale('symlog')
+plt.grid(True)
 plt.show()
