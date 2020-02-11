630d4d7cc62d4a1c42f00ef7acc5f1f0532882f3
Tim Day <timday@bottlenose.demon.co.uk>
2020-02-11 18:42:04 +0000

    Seems to work better

--- experiment2.py
+++ experiment2.py
@@ -4,6 +4,7 @@
 # But really want to integrate over a range of previous t
 # which would be more like https://stackoverflow.com/a/52260839/24283
 
+import math
 import numpy as np
 from ddeint import ddeint
 import matplotlib.pyplot as plt
@@ -15,25 +16,31 @@ import matplotlib.pyplot as plt
 
 w=np.array([min(0.5+i,6.5-i) for i in range(7)])
 w=w/np.sum(w)
+assert math.fabs(np.sum(w)-1.0)<0.0001
 
 # Needs to return derivatives
 def model(Y,t):
 
-    w0=np.sum(w*np.array([Y(t-7-d)[0] for d in range(7)]))
-    ww0=np.sum(w*w*np.array([Y(t-7-d)[0] for d in range(7)]))
-    w1=np.sum(w*np.array([Y(t-7-d)[1] for d in range(7)]))
-    ww1=np.sum(w*w*np.array([Y(t-7-d)[1] for d in range(7)]))
+    i=0.0
+    if 0.0<=t and t<1.0:
+        i=1.0-t
     
+    w0=np.sum(w*np.array([Y(t-7.5-d)[0] for d in range(7)]))
+    w1=np.sum(w*np.array([Y(t-7.5-d)[1] for d in range(7)]))
+
     return np.array([
-            w1 - min(1.0,Y(t)[0])*ww0,  # Huh?  Double multiply by w and w[d] in subtracted expression here.
-        1.2*w0 - min(1.0,Y(t)[1])*ww1,  # Huh?  Double multiply by w and w[d] in subtracted expression here.
+            w1 - w0,
+        1.2*Y(t)[0] - (w1-i),
         0.05*w0
     ])
 
 def values_before_zero(t):
-    return np.array([1.0,0.0,0.0])
+    i=0.0
+    if -1.0<t and t<0.0:
+        i=1.0+t
+    return np.array([0.0,i,0.0])
 
-ts=np.arange(120)
+ts=np.arange(60)
 
 ys=ddeint(model,values_before_zero,ts)
 
