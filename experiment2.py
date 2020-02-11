6255b7e53912a7829581ac1e28f8b37079a4182e
timday <timday@bottlenose.demon.co.uk>
2020-02-11 20:13:12 +0000

    Works better

--- experiment2.py
+++ experiment2.py
@@ -10,9 +10,10 @@ from ddeint import ddeint
 import matplotlib.pyplot as plt
 
 # Elements:
-#  0: Number contagious
-#  1: Number incubating 
-#  2: Number observed
+#  0: Initial infectious impulse
+#  1: Number incubating
+#  2: Number contagious 
+#  3: Number observed
 
 w=np.array([min(0.5+i,6.5-i) for i in range(7)])
 w=w/np.sum(w)
@@ -21,32 +22,26 @@ assert math.fabs(np.sum(w)-1.0)<0.0001
 # Needs to return derivatives
 def model(Y,t):
 
-    i=0.0
-    if 0.0<=t and t<1.0:
-        i=1.0-t
-    
-    w0=np.sum(w*np.array([Y(t-7.5-d)[0] for d in range(7)]))
     w1=np.sum(w*np.array([Y(t-7.5-d)[1] for d in range(7)]))
+    w2=np.sum(w*np.array([Y(t-7.5-d)[2] for d in range(7)]))
 
     return np.array([
-            w1 - w0,
-        1.2*Y(t)[0] - (w1-i),
-        0.05*w0
+        -Y(t)[0]/7.0,                 # Initial impulse decays away
+        1.2*(Y(t)[2]+Y(t)[0]) - w1,   # Infectious
+        w1 - w2,                      # Contagious
+        0.05*Y(t)[2]                  # Observed
     ])
 
 def values_before_zero(t):
-    i=0.0
-    if -1.0<t and t<0.0:
-        i=1.0+t
-    return np.array([0.0,i,0.0])
+    return np.array([1.0,0.0,0.0,0.0])
 
-ts=np.arange(60)
-
+ts=np.arange(90)
 ys=ddeint(model,values_before_zero,ts)
 
-plt.plot(ts,ys[:,0],color='green')
+plt.plot(ts,ys[:,0],color='black')
 plt.plot(ts,ys[:,1],color='blue')
-plt.plot(ts,ys[:,2],color='red')
+plt.plot(ts,ys[:,2],color='green')
+plt.plot(ts,ys[:,3],color='red')
 plt.yscale('symlog')
 plt.grid(True)
 plt.show()
