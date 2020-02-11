5a341b0d1c8bb8ef85b415adad117ca56a1385bc
timday <timday@bottlenose.demon.co.uk>
2020-02-11 00:18:57 +0000

    Promising basis for replacing numeric simulations.

--- experiment2.py
+++ experiment2.py
@@ -9,35 +9,31 @@ from ddeint import ddeint
 import matplotlib.pyplot as plt
 
 # Elements:
-#  0: Number actually contagious
-#  1: Number becoming contagious = proportional to number actually contagious 7 days ago (incubation time)
-#  2: Number recovering = number becoming contagious 7 days ago (contagious time)
-#  3: Number observed = Proportion of becoming contagious
+#  0: Number contagious
+#  1: Number incubating 
+#  4: Number observed
 
 w=np.array([min(0.5+i,6.5-i) for i in range(7)])
 w=w/np.sum(w)
 
+# Needs to return derivatives
 def model(Y,t):
-    fresh=1.2*np.sum(w*np.array([Y(t-7-d)[0] for d in range(7)]))  # Fresh contagious cases based on contagious cases 7-13 days ago
-    recov=np.sum(w*np.array([Y(t-7-d)[1] for d in range(7)]))      # Recovering cases based on fresh cases 7-13 days ago.
     return np.array([
-        Y(t)[0]+fresh-recov,
-        fresh,
-        recov,
-        Y(t)[3]+0.05*fresh
+             np.sum(w*np.array([Y(t-7-d)[1] for d in range(7)])) - min(1.0,Y(t)[0])*np.sum(w*np.array([w[d]*Y(t-7-d)[0] for d in range(7)])),
+        1.2 *np.sum(w*np.array([Y(t-7-d)[0] for d in range(7)])) - min(1.0,Y(t)[1])*np.sum(w*np.array([w[d]*Y(t-7-d)[1] for d in range(7)])),
+        0.05*np.sum(w*np.array([Y(t-7-d)[1] for d in range(7)]))
     ])
 
 def values_before_zero(t):
-    return np.array([1.0,0.0,0.0,0.0])
+    return np.array([1.0,0.0,0.0])
 
-ts=np.arange(60)
+ts=np.arange(120)
 
 ys=ddeint(model,values_before_zero,ts)
 
 plt.plot(ts,ys[:,0],color='green')
-plt.plot(ts,ys[:,1],color='black')
-plt.plot(ts,ys[:,2],color='blue')
-plt.plot(ts,ys[:,3],color='red')
+plt.plot(ts,ys[:,1],color='blue')
+plt.plot(ts,ys[:,2],color='red')
 plt.yscale('symlog')
 plt.grid(True)
 plt.show()
