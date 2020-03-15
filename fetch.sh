92102bedaa658b408b5b089b02de551a07a4adc8
Tim Day <timday@bottlenose.demon.co.uk>
2020-03-15 16:18:21 +0000

    Fetch other data.

---
+++ fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+(cd data ; wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv )
+
+(cd data ; wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv )
+
+(cd data ; wget https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv )
+
