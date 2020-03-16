fa8b3377bb6ae780b861a2b4a8f81f258e6695be
Tim Day <timday@bottlenose.demon.co.uk>
2020-03-16 11:45:15 +0000

    Get China data from csv file

--- JHUData.py
+++ JHUData.py
@@ -11,20 +11,18 @@ basedate=mdates.date2num(datetime.datetime(2020,1,20))
 # Data from https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6
 # Use inspect element on graph to get precise numbers
 # Starts 2020-01-20:
-china=np.array([278,326,547,639,916,1979,2737,4409,5970,7678,9658,11221,14341,17187,19693,23680,27409,30553,34075,36778,39790,42306,44327,44699,59832,66292,68347,70446,72364,74139,74546,74999,75472,76922,76938,77152,77660,78065,78498,78824,79251,79826,80026,80151,80271,80422,80573,80652,80699,80735,80757,80921,80932,80945,80977,80003],dtype=np.float64)
+#china=np.array([278,326,547,639,916,1979,2737,4409,5970,7678,9658,11221,14341,17187,19693,23680,27409,30553,34075,36778,39790,42306,44327,44699,59832,66292,68347,70446,72364,74139,74546,74999,75472,76922,76938,77152,77660,78065,78498,78824,79251,79826,80026,80151,80271,80422,80573,80652,80699,80735,80757,80921,80932,80945,80977,80003],dtype=np.float64)
 other=np.array([  4,  6,  8, 14, 25,  40,  57,  64,  87, 105, 118,  153,  173,  183,  188,  212,  227,  265,  317,  343,  361,  457,  476,  523,  538,  595,  685,  780,  896, 1013, 1095, 1200, 1371, 1677, 2047, 2418, 2755, 3332, 4258, 5300, 6762, 8545,10283,12693,14853,17464,21227,25184,29136,32847,37825,44944,47411,63569,75122,81716],dtype=np.float64)
-
-assert len(china)==len(other)
 
 # Keys keep changing.
 # Republic of Korea -> Korea, South
 # Iran (Islamic Republic of) -> Iran
 
-# Starts 2020-01-22, so zero pad.
+# csv file starts 2020-01-22, so zero pad, except for China which has a couple of extra days data
 csvfile=open('data/time_series_19-covid-Confirmed.csv','rb')
 reader=csv.reader(csvfile)
 timeseries={}
-interesting=frozenset(['UK','Italy','South Korea','US','Iran','France','Germany','Spain','Japan','Switzerland','Netherlands','Sweden','Norway','Denmark','Belgium','Austria'])
+interesting=frozenset(['China','UK','Italy','South Korea','US','Iran','France','Germany','Spain','Japan','Switzerland','Netherlands','Sweden','Norway','Denmark','Belgium','Austria'])
 for row in reader:
     where=row[1]
     if where=='Republic of Korea' or where=='Korea, South':
@@ -36,16 +34,20 @@ for row in reader:
 
     if where in interesting:
         if not where in timeseries:
-            timeseries[where]=np.zeros(2+len(row[4:]))
-        timeseries[where]+=np.concatenate([np.zeros(2),np.array(map(lambda x: int(x),row[4:]),dtype=np.float64)])
+            if where=='China':
+                pad=np.array([278.0,326.0])
+            else:
+                pad=np.array([0.0,0.0])
+            timeseries[where]=np.concatenate([pad,np.zeros(len(row[4:]))])
+        timeseries[where]+=np.concatenate([np.array([0.0,0.0]),np.array(map(lambda x: int(x),row[4:]),dtype=np.float64)])
 
-timeseries['China']=china
+#timeseries['China']=china
 timeseries['Other']=other
-timeseries['Total']=china+other
+timeseries['Total']=timeseries['China']+other
 
 timeseriesKeys=['Total','Other','China','Iran','South Korea','Italy','France','Spain','Germany','US','Japan','Netherlands','Switzerland','UK','Sweden','Norway','Belgium','Denmark','Austria']
 for k in timeseriesKeys:
-    assert len(timeseries[k])==len(china)
+    assert len(timeseries[k])==len(timeseries['China'])
 
 timeseriesKeys.sort(key=lambda k: timeseries[k][-1],reverse=True)
 
