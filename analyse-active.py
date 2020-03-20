f5b4127ce476cb909d3381e5537cdbe7a03fe78c
timday <timday@bottlenose.demon.co.uk>
2020-03-20 23:05:19 +0000

    Tweaks.

--- analyse-active.py
+++ analyse-active.py
@@ -10,44 +10,55 @@ from JHUData import *
 
 timeseriesKeys,timeseries=getJHUData(True,True)
 
-fig=plt.figure()
+def active(k):
 
+    casesTotal=timeseries[0][k]
+    casesRecovered=timeseries[1][k]
+    casesFatal=timeseries[2][k]
+    
+    casesActive=casesTotal-(casesRecovered+casesFatal)
+    
+    casesActive[casesActive<30.0]=np.nan
+
+    return casesActive
+    
 for p in range(4):
 
-    plt.subplot(2,2,1+p)
+    fig=plt.figure()
 
     for k in timeseriesKeys:
 
-        casesTotal=timeseries[0][k]
-        casesRecovered=timeseries[1][k]
-        casesFatal=timeseries[2][k]
-    
-        casesActive=casesTotal-(casesRecovered+casesFatal)
-    
-        casesActive[casesActive<30.0]=np.nan
+        casesActive=active(k)
 
         if p==2 or p==3:
            casesActive=casesActive/populations[k]
 
-        plt.plot(np.arange(len(casesActive)),casesActive,label=descriptions[k],color=colors[k],linewidth=3.0*widthScale[k])
-    
+        plt.plot(np.arange(len(casesActive)),casesActive,label=descriptions[k],color=colors[k],linewidth=5.0*widthScale[k])
+
+        plt.text(
+            len(casesActive)-0.5,
+            casesActive[-1],
+            descriptions[k],
+            horizontalalignment='left',
+            verticalalignment='center',
+            fontdict={'size':10,'alpha':0.75,'weight':'bold','color':colors[k]}
+        )
+
     if p==0 or p==2:
        plt.yscale('log')
 
-    if p==2:
-       plt.legend(loc='upper left',fontsize='x-small')
-
+    plt.xlim(left=0,right=len(casesActive)-1)
     plt.gca().xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(7.0))
     plt.title(
         {
-            0: 'Active cases, log-scale',
-            1: 'Active cases',
-            2: 'Active cases, proportion of population, log-scale',
-            3: 'Active cases, proportion of population'
+            0: 'Active cases (from $\geq 30$), log-scale',
+            1: 'Active cases (from $\geq 30$)',
+            2: 'Active cases (from $\geq 30$), proportion of population, log-scale',
+            3: 'Active cases (from $\geq 30$), proportion of population'
         }[p]
     )
 
-plt.suptitle('Active cases ($\geq 30$)')
+    plt.legend(loc='upper left',fontsize='medium')
 
 #def on_resize(event):
 #    fig.tight_layout()
