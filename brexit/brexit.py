11a50307029e08d8f41a6381b96817da0c2f3fac
Tim Day <timday@bottlenose.demon.co.uk>
2020-03-23 21:00:09 +0000

    WIP.  Updated data.

--- brexit/brexit.py
+++ brexit/brexit.py
@@ -32,7 +32,7 @@ def value(s):
         return float(s)
 
 def getCases():
-    csvfile=open('data/covid-19-cases-uk.csv')
+    csvfile=open('data/covid-19-cases-uk.csv')  # Update from https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-cases-uk.csv
     reader=csv.reader(csvfile)
     timeseries=defaultdict(list)
     areas=defaultdict(set)
@@ -63,11 +63,50 @@ def getCases():
         
     return areas,timeseries
 
+def getCodeRewrites():
 
-def getVotes(interesting,areas):
+    codes={}
+    
+    # First, read the England and Wales data
+    csvfile=open('data/Lower_Tier_Local_Authority_to_Upper_Tier_Local_Authority_December_2017_Lookup_in_England_and_Wales.csv')
+    reader=csv.reader(csvfile)
+    firstRow=True
+    for row in reader:
+        if firstRow:
+            firstRow=False
+            continue
+
+        lower=row[0]
+        upper=row[2]
+
+        assert not lower in codes
+
+        codes[lower]=upper
+
+    # Now read the Scottish data
+    csvfile=open('data/Datazone2011Lookup.csv','rb')
+    reader=csv.reader(csvfile)
+    firstRow=True
+    for row in reader:
+        if firstRow:
+            firstRow=False
+            continue
+
+        council=row[5]
+        healthboard=row[6]
+
+        if council in codes:
+            assert codes[council]==healthboard  # Check no contradictions
+        else:
+            codes[council]=healthboard
+
+    return codes
+
+def getVotesLeave(codeRewrites,interesting):
     csvfile=open('data/EU-referendum-result-data.csv')
     reader=csv.reader(csvfile)
-    votes={}
+    votesTotal=defaultdict(float)
+    votesLeave=defaultdict(float)
     firstRow=True
     for row in reader:
         if firstRow:
@@ -77,93 +116,24 @@ def getVotes(interesting,areas):
             firstRow=False
             continue
 
-        if row[3] in interesting:
-            votes[row[3]]=value(row[12])/value(row[10])
+        code=row[3]
 
+        if code in codeRewrites and not code in interesting:
+            code=codeRewrites[code]
+        
+        votesTotal[code]+=value(row[10])
+        votesLeave[code]+=value(row[12])
 
-    print 'Missing votes for',[areas[k] for k in interesting if not k in votes]
-            
-    return votes
+    return votesTotal,votesLeave
 
 areas,timeseries=getCases()
 #print areas
 #print timeseries
-votes=getVotes(frozenset(areas.keys()),areas)
 
-# TODO: Need to figure out things like Devon and Surrey which seem to be split in referendum data
-# Need to link to lower tier authorities.  UTLA E10000030 includes a bunch of lower tier stuff
-# This looks useful: https://geoportal.statistics.gov.uk/datasets/lower-tier-local-authority-to-upper-tier-local-authority-december-2017-lookup-in-england-and-wales
+codeRewrites=getCodeRewrites()
+votesTotal,votesLeave=getVotesLeave(codeRewrites,frozenset(areas.keys())
 
-print votes
-
-print len(votes),len(areas)
-
-
-exit()
-
-# Data captured Upper Tier Local Authorities (UTLA) and NHS Regions tab
-#   2020-03-21
-#   2020-03-22
-cases={
-    'London'                   :[1965,2189],
-    'South East'               :[ 492, 624],
-    'Midlands'                 :[ 491, 536],
-    'North West'               :[ 312, 390],
-    'North East\nand Yorkshire':[ 298, 368],
-    'East of England'          :[ 221, 274],
-    'South West'               :[ 216, 242]
-}
-
-# From wikipedia https://en.wikipedia.org/wiki/Results_of_the_2016_United_Kingdom_European_Union_membership_referendum#Greater_London
-# Just sort the above from least-to-most Leave
-order=[
-    'London',
-    'South East',
-    'South West',                 
-    'North West',                 
-    'East of England',            
-    'North East\nand Yorkshire',
-    'Midlands'
-]
-
-def expand(s):
-    if s=='London':
-        return s+'\n(Most Remain)'
-    elif s=='Midlands':
-        return s+'\n(Most Leave)'
-    else:
-        return s
-
-growth=[100.0*(float(cases[k][1])/float(cases[k][0])-1.0) for k in order]
-
-pos=np.arange(len(order))
-
-matplotlib.rcParams['font.sans-serif'] = "Comic Sans MS"
-matplotlib.rcParams['font.family'] = "sans-serif"
-
-fig=plt.figure(figsize=(8,6))
-
-plt.bar(
-    pos,
-    growth
-)
-
-ax=plt.gca()
-vals=ax.get_yticks()
-ax.set_yticklabels(['{:,.1f}%'.format(x) for x in vals])
-ax.set_xticks(pos)
-ax.set_xticklabels(map(expand,order))
-
-for tick in ax.get_xticklabels():
-    #tick.set_fontname("Comic Sans MS")
-    tick.set_fontsize(8)
-
-for tick in ax.get_yticklabels():
-    #tick.set_fontname("Comic Sans MS")
-    tick.set_fontsize(8)
-
-plt.title('Virus cases growth rate 2020/03/21 - 2020/03/22 by NHS Region')
-
-plt.savefig('output/brexit.png',dpi=96)
-
-plt.show()
+for k in timeseries.keys():
+    if not k in votesTotal:
+        print 'No votes for',k
+    print k,areas[k],timeseries[k][-1],100.0*votesLeave[k]/votesTotal[k]
