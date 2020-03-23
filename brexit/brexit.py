7fd9fba95a12712e0e7d94cbc6a5b6b9d7839ee6
timday <timday@bottlenose.demon.co.uk>
2020-03-23 08:47:33 +0000

    Notes

--- brexit/brexit.py
+++ brexit/brexit.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+import csv
+import math
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 
 # Dashboard at https://www.arcgis.com/apps/opsdashboard/index.html#/f94c3c90da5b4e9f9a0b19484dd4bb14
 # Seems to be some csv files at https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases
@@ -16,6 +20,82 @@ import numpy as np
 
 # Referendum results from https://data.london.gov.uk/dataset/eu-referendum-results
 #   Downloaded https://data.london.gov.uk/download/eu-referendum-results/52dccf67-a2ab-4f43-a6ba-894aaeef169e/EU-referendum-result-data.csv
+
+def value(s):
+    if s=='1 to 4':
+        return 2.5
+    else:
+        return float(s)
+
+def getCases():
+    csvfile=open('data/covid-19-cases-uk.csv')
+    reader=csv.reader(csvfile)
+    timeseries=defaultdict(list)
+    areas=defaultdict(set)
+    firstRow=True
+    for row in reader:
+        if firstRow:
+            firstRow=False
+            continue
+
+        code=row[2]
+        area=row[3]
+        cases=value(row[4])
+
+        if code=='':
+            continue
+
+        if area=='Orkney':
+            assert code=='S08000024'
+            code='S08000025'
+
+        if area=='Western Isles':
+            assert code=='S08000030'
+            code='S08000028'
+        
+        timeseries[code].append(cases)
+
+        areas[code].add(area)
+        
+    return areas,timeseries
+
+
+def getVotes(interesting,areas):
+    csvfile=open('data/EU-referendum-result-data.csv')
+    reader=csv.reader(csvfile)
+    votes={}
+    firstRow=True
+    for row in reader:
+        if firstRow:
+            assert row[10]=='Valid_Votes'
+            assert row[11]=='Remain'
+            assert row[12]=='Leave'
+            firstRow=False
+            continue
+
+        if row[3] in interesting:
+            votes[row[3]]=value(row[12])/value(row[10])
+
+
+    print 'Missing votes for',[areas[k] for k in interesting if not k in votes]
+            
+    return votes
+
+areas,timeseries=getCases()
+#print areas
+#print timeseries
+votes=getVotes(frozenset(areas.keys()),areas)
+
+# TODO: Need to figure out things like Devon and Surrey which seem to be split in referendum data
+# Need to link to lower tier authorities.  UTLA E10000030 includes a bunch of lower tier stuff
+# This looks useful: https://geoportal.statistics.gov.uk/datasets/lower-tier-local-authority-to-upper-tier-local-authority-december-2017-lookup-in-england-and-wales
+
+print votes
+
+print len(votes),len(areas)
+
+
+exit()
 
 # Data captured Upper Tier Local Authorities (UTLA) and NHS Regions tab
 #   2020-03-21
