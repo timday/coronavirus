c3f3c364a42fb215b19e560c700ab681beae8dca
timday <timday@bottlenose.demon.co.uk>
2020-03-24 21:52:23 +0000

    Tinker with uk scanning.

--- brexit/uk.py
+++ brexit/uk.py
@@ -18,6 +18,7 @@ reader=csv.reader(csvfile)
 firstRow=True
 
 timeseries={}
+days=set()
 
 for row in reader:
     if firstRow:
@@ -29,16 +30,40 @@ for row in reader:
     cases=value(row[4])
 
     if not area in timeseries:
-        timeseries[area]=[]
-    timeseries[area].append(cases)
+        timeseries[area]={}
+    timeseries[area][date]=cases
 
-print 'Top 10 case counts'
-for k in sorted(timeseries.keys(),key=lambda k: timeseries[k][-1],reverse=True)[:10]:
-    print k,timeseries[k][-1]
+    days.add(date)
+
+days=sorted(list(days))
+
+days=days[-8:-1]
+assert len(days)==7
+
+print 'Date range',days[0],days[-1]
+
+incomplete=set()
+print 'Incomplete timeseries '.format(days)
+for k in timeseries.keys():
+    has=set(timeseries[k].keys()).intersection(days)
+    if len(has)!=len(days):
+        print '  ',k,'missing',sorted(list(set(days).difference(has)))
+        incomplete.add(k)
 
 print
 
-growth={k:(timeseries[k][-1]/timeseries[k][-1-7])**(1.0/7.0) for k in timeseries.keys() if len(timeseries[k])>=8 and timeseries[k][-1-7]>0.0}
+usable=[k for k in sorted(timeseries.keys()) if not k in incomplete]
+
+print 'Top 10 case counts'
+for k in sorted(usable,key=lambda k: timeseries[k][days[-1]],reverse=True)[:10]:
+    print '  ',k,timeseries[k][days[-1]]
+
+print
+
+growth={
+    k:(timeseries[k][days[-1]]/timeseries[k][days[0]])**(1.0/7.0)
+    for k in usable if len(timeseries[k])>=8 and timeseries[k][days[0]]>0.0
+}
 print 'Top 10 growth'
 for k in sorted(growth.keys(),key=lambda k: growth[k],reverse=True)[:10]:
-    print k,100.0*(growth[k]-1.0)
+    print '  ',k,100.0*(growth[k]-1.0)
