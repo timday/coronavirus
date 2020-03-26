df85b75e95f8d942412fbf265ab52ac5d9a09e23
Tim Day <timday@bottlenose.demon.co.uk>
2020-03-26 16:05:07 +0000

    Factor out UK Covid reader.

---
+++ brexit/UKCovid19Data.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import csv
+import datetime
+
+def value(s):
+    if s=='1 to 4':
+        return 2.5
+    else:
+        return float(s)
+
+# NB Due to messing around in Wales switching between Local Authority and Health Board, probably can only trust stuff from the 21st March for Wales.
+# See https://github.com/tomwhite/covid-19-uk-data/blob/master/README.md
+
+def getUKCovid19Data(nation,window):
+
+    csvfile=open('data/covid-19-cases-uk.csv')  # Update from https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-cases-uk.csv
+    reader=csv.reader(csvfile)
+    firstRow=True
+
+    timeseries={}
+    days=set()
+    codes={}
+
+    for row in reader:
+        if firstRow:
+            firstRow=False
+            continue
+        
+        where=row[1]
+        if where!=nation and nation!=None:
+            continue
+
+        ymd=map(int,row[0].split('-'))
+        date=datetime.date(*ymd)
+
+        code=row[2]
+        area=row[3]
+
+        codes[code]=area  # Taking the last one seems to be OK.  Some instability earlier.  https://github.com/tomwhite/covid-19-uk-data/issues/15
+        
+        cases=value(row[4])
+
+        if not code in timeseries:
+            timeseries[code]={}
+        timeseries[code][date]=cases
+
+        days.add(date)
+
+    days=sorted(list(days))[-window:]
+    assert len(days)==window
+    
+    def trim(ts):
+        return [it[1] for it in sorted(ts.items(),key=lambda x: x[0])][-window:]
+
+    timeseries={a:trim(timeseries[a]) for a in timeseries.keys()}
+
+    timeseries={a:timeseries[a] for a in timeseries.keys() if len(timeseries[a])==window}
+        
+    return timeseries,days,codes
+                    
--- brexit/uk.py
+++ brexit/uk.py
@@ -1,66 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
-import datetime
 import math
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-def value(s):
-    if s=='1 to 4':
-        return 2.5
-    else:
-        return float(s)
+import UKCovid19Data
 
-# NB Due to messing around in Wales switching between Local Authority and Health Board, probably can only trust stuff from the 21st March for Wales.
-# See https://github.com/tomwhite/covid-19-uk-data/blob/master/README.md
-
-def getUK(nation,window):
-
-    csvfile=open('data/covid-19-cases-uk.csv')  # Update from https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-cases-uk.csv
-    reader=csv.reader(csvfile)
-    firstRow=True
-
-    timeseries={}
-    days=set()
-
-    for row in reader:
-        if firstRow:
-            firstRow=False
-            continue
-        
-        where=row[1]
-        if where!=nation and nation!=None:
-            continue
-
-        ymd=map(int,row[0].split('-'))
-        date=datetime.date(*ymd)
-            
-        area=row[3]
-        cases=value(row[4])
-
-        if not area in timeseries:
-            timeseries[area]={}
-        timeseries[area][date]=cases
-
-        days.add(date)
-
-    days=sorted(list(days))[-window:]
-    assert len(days)==window
+for what in [('England',7),('Scotland',7),('Wales',5),(None,5)]:
     
-    def trim(ts):
-        return [it[1] for it in sorted(ts.items(),key=lambda x: x[0])][-window:]
-
-    timeseries={a:trim(timeseries[a]) for a in timeseries.keys()}
-
-    timeseries={a:timeseries[a] for a in timeseries.keys() if len(timeseries[a])==window}
-        
-    return timeseries,days
-                    
-for what in [('England',14),('Scotland',14),('Wales',4),(None,4)]:
-    timeseries,days=getUK(*what)
+    timeseries,days,codes=UKCovid19Data.getUKCovid19Data(*what)
 
     print '------'
     print what[0],days[0],days[-1],len(days)
@@ -69,7 +19,7 @@ for what in [('England',14),('Scotland',14),('Wales',4),(None,4)]:
 
     print 'Top 10 case counts'
     for k in sorted(timeseries,key=lambda k: timeseries[k][-1],reverse=True)[:10]:
-        print '  {:32s}: {:d}'.format(k,int(timeseries[k][-1]))
+        print '  {:32s}: {:d}'.format(codes[k],int(timeseries[k][-1]))
     
     print
 
@@ -80,6 +30,6 @@ for what in [('England',14),('Scotland',14),('Wales',4),(None,4)]:
     }
     print 'Top 10 growth ({} days)'.format(window)
     for k in sorted(growth.keys(),key=lambda k: growth[k],reverse=True)[:10]:
-        print '  {:32s}: {:.1f}%'.format(k,100.0*(growth[k]-1.0))
+        print '  {:32s}: {:.1f}%'.format(codes[k],100.0*(growth[k]-1.0))
 
     print
