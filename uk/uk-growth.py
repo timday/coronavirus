068ee21706d23a67192257aa7cb66c6314a6926d
Tim Day <timday@bottlenose.demon.co.uk>
2020-03-29 22:50:56 +0100

    Compute growth rate correctly.

--- uk/uk-growth.py
+++ uk/uk-growth.py
@@ -8,12 +8,12 @@ import UKCovid19Data
 
 for what in [('England',7,None),('Scotland',7,None),('Wales',7,None),(None,7,None)]:
     
-    timeseries,days,codes=UKCovid19Data.getUKCovid19Data(*what)
+    timeseries,days,codes=UKCovid19Data.getUKCovid19Data(what[0],what[1]+1,what[2])
 
     print '------'
     print what[0],days[0],days[-1],len(days)
 
-    assert len(days)==what[1]
+    assert len(days)==what[1]+1
 
     print 'Top 20 case counts'
     for k in sorted(timeseries,key=lambda k: timeseries[k][-1],reverse=True)[:20]:
@@ -21,9 +21,9 @@ for what in [('England',7,None),('Scotland',7,None),('Wales',7,None),(None,7,Non
     
     print
 
-    window=min(what[1],7)
+    window=what[1]
     growth={
-        k:(timeseries[k][-1]/timeseries[k][-window])**(1.0/window)
+        k:(timeseries[k][-1]/timeseries[k][-1-window])**(1.0/window)
         for k in timeseries if timeseries[k][-window]>0.0
     }
     print 'Top growth ({} days, {} to {})'.format(window,days[0],days[-1])
