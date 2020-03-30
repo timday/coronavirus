f7b60ef5fbf907da35dfd4820fbc697cf59c8a48
Tim Day <timday@bottlenose.demon.co.uk>
2020-03-30 11:09:02 +0100

    Log-scale chart

--- uk/uk-cases.py
+++ uk/uk-cases.py
@@ -10,51 +10,65 @@ import numpy as np
 
 import UKCovid19Data
 
-fig=plt.figure(figsize=(16,9))
+for chart in [0,1]:
 
-mdayslo=None
-mdayshi=None
-
-for what in [('England',None,datetime.date(2020,3,7)),('Scotland',None,datetime.date(2020,3,7)),('Wales',None,datetime.date(2020,3,21))]:
+    fig=plt.figure(figsize=(16,9))
     
-    timeseries,days,codes=UKCovid19Data.getUKCovid19Data(*what)
-
-    mdays=[mdates.date2num(d) for d in days]
-
-    if mdayslo==None:
-        mdayslo=mdays[0]
-    else:
-        mdayslo=min(mdayslo,mdays[0])
-
-    if mdayshi==None:
-        mdayshi=mdays[-1]
-    else:
-        mdayshi=max(mdayshi,mdays[-1])
-
-    colors={'E':'tab:gray','W':'tab:red','S':'tab:blue'}
-    labels={'E':'England','W':'Wales','S':'Scotland'}
-    for k in timeseries.keys():
-        plt.plot(mdays,timeseries[k],color=colors[k[0]],alpha=0.75)
-        plt.text(mdays[-1]+0.05,timeseries[k][-1],codes[k],horizontalalignment='left',verticalalignment='center',fontdict={'size':8,'alpha':0.75,'weight':'bold','color':colors[k[0]]})
+    mdayslo=None
+    mdayshi=None
     
-legends=[matplotlib.patches.Patch(color=colors[k],label=labels[k]) for k in ['E','W','S']]
-plt.legend(handles=legends)
-
-plt.legend(loc='upper left')
+    for what in [('England',None,datetime.date(2020,3,7)),('Scotland',None,datetime.date(2020,3,7)),('Wales',None,datetime.date(2020,3,21))]:
+        
+        timeseries,days,codes=UKCovid19Data.getUKCovid19Data(*what)
+        
+        mdays=[mdates.date2num(d) for d in days]
     
-plt.subplots_adjust(right=0.8)
+        if mdayslo==None:
+            mdayslo=mdays[0]
+        else:
+            mdayslo=min(mdayslo,mdays[0])
+    
+        if mdayshi==None:
+            mdayshi=mdays[-1]
+        else:
+            mdayshi=max(mdayshi,mdays[-1])
+    
+        colors={'E':'tab:gray','W':'tab:red','S':'tab:blue'}
+        labels={'E':'England','W':'Wales','S':'Scotland'}
+        for k in timeseries.keys():
+            cases=np.array([y for y in timeseries[k]])
+            if chart==1:
+                cases[cases<10.0]=np.nan
 
-plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=mdates.MO))
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-plt.xticks(rotation=75,fontsize=10)
-
-plt.xlim(left=mdayslo,right=mdayshi)
-plt.ylim(bottom=0.0)
-#plt.yscale('symlog')
-plt.gca().set_ylabel('Cumulative cases')
-
-plt.title('Cases by UTLA')
-
-plt.savefig('output/cases.png',dpi=96)
+            plt.plot(mdays,cases,color=colors[k[0]],alpha=0.75)
+            if not np.isnan(cases[-1]):
+                plt.text(mdays[-1]+0.05,cases[-1],codes[k],horizontalalignment='left',verticalalignment='center',fontdict={'size':8,'alpha':0.75,'weight':'bold','color':colors[k[0]]})
+        
+    legends=[matplotlib.patches.Patch(color=colors[k],label=labels[k]) for k in ['E','W','S']]
+    plt.legend(handles=legends)
+    
+    plt.legend(loc='upper left')
+        
+    plt.subplots_adjust(right=0.8)
+    
+    plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=mdates.MO))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.xticks(rotation=75,fontsize=10)
+    
+    plt.xlim(left=mdayslo,right=mdayshi)
+    plt.ylim(bottom={0:0.0,1:10.0}[chart])
+    plt.gca().set_ylabel('Cumulative cases')
+    if chart==1:
+        plt.yscale('symlog')
+    
+    plt.title({0:'Cases by UTLA',1:'Cases by UTLA (log scale) from $\geq10$'}[chart])
+    
+    plt.savefig(
+        {
+            0:'output/cases.png',
+            1:'output/cases-log.png'
+        }[chart]
+        ,dpi=96
+    )
 
 plt.show()
