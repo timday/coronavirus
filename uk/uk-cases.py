8e879df3a60760b4b01bcd38f35c7c163c09685f
Tim Day <timday@bottlenose.demon.co.uk>
2020-03-30 13:18:00 +0100

    Colour by region

--- uk/uk-cases.py
+++ uk/uk-cases.py
@@ -10,6 +10,33 @@ import numpy as np
 
 import UKCovid19Data
 
+regions=UKCovid19Data.getUKRegions()
+
+def whichRegion(code):
+    if code[0]=='E':
+        return regions[code]
+    elif code[0]=='W':
+        return 'Wales'
+    elif code[0]=='S':
+        return 'Scotland'
+    else:
+        assert False
+        return '???'
+
+colorsByRegion={
+    'East Midlands'            :'tab:olive',
+    'East of England'          :'tab:pink',
+    'London'                   :'black',
+    'North East'               :'tab:cyan',
+    'North West'               :'tab:purple',
+    'South East'               :'tab:gray',
+    'South West'               :'tab:orange',
+    'West Midlands'            :'tab:green',
+    'Yorkshire and The Humber' :'tab:brown',
+    'Wales'                    :'tab:red',
+    'Scotland'                 :'tab:blue'
+}
+
 for chart in [0,1]:
 
     fig=plt.figure(figsize=(16,9))
@@ -20,9 +47,9 @@ for chart in [0,1]:
     for what in [('England',None,datetime.date(2020,3,7)),('Scotland',None,datetime.date(2020,3,7)),('Wales',None,datetime.date(2020,3,21))]:
         
         timeseries,days,codes=UKCovid19Data.getUKCovid19Data(*what)
-        
+
         mdays=[mdates.date2num(d) for d in days]
-    
+        
         if mdayslo==None:
             mdayslo=mdays[0]
         else:
@@ -33,18 +60,18 @@ for chart in [0,1]:
         else:
             mdayshi=max(mdayshi,mdays[-1])
     
-        colors={'E':'tab:gray','W':'tab:red','S':'tab:blue'}
-        labels={'E':'England','W':'Wales','S':'Scotland'}
-        for k in timeseries.keys():
+        for k in sorted(timeseries.keys(),key=lambda k: timeseries[k][-1]):   # Plot highest current case counts last
             cases=np.array([y for y in timeseries[k]])
             if chart==1:
                 cases[cases<10.0]=np.nan
 
-            plt.plot(mdays,cases,color=colors[k[0]],alpha=0.75)
+            region=whichRegion(k)
+                
+            plt.plot(mdays,cases,color=colorsByRegion[region],alpha=0.8)
             if not np.isnan(cases[-1]):
-                plt.text(mdays[-1]+0.05,cases[-1],codes[k],horizontalalignment='left',verticalalignment='center',fontdict={'size':8,'alpha':0.75,'weight':'bold','color':colors[k[0]]})
+                plt.text(mdays[-1]+0.05,cases[-1],codes[k],horizontalalignment='left',verticalalignment='center',fontdict={'size':8,'alpha':0.75,'weight':'bold','color':colorsByRegion[region]})
         
-    legends=[matplotlib.patches.Patch(color=colors[k],label=labels[k]) for k in ['E','W','S']]
+    legends=[matplotlib.patches.Patch(color=colorsByRegion[k],label=k) for k in sorted(colorsByRegion.keys())]
     plt.legend(handles=legends)
     
     plt.legend(loc='upper left')
