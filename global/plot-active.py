8fa6675e1c62af20d3e0b1f96dfcd2e30a04ff79
Tim Day <timday@bottlenose.demon.co.uk>
2020-04-02 15:02:21 +0100

    Fis issue #1

--- global/plot-active.py
+++ global/plot-active.py
@@ -38,10 +38,11 @@ for p in range(4):
         if p==2 or p==3:
            casesActive=casesActive/populations[k]
 
-        plt.plot([t+basedate for t in range(len(casesActive))],casesActive,label=descriptions[k],color=colors[k],linewidth=3.0*widthScale[k])
+        x=[t+basedate for t in range(len(casesActive))]
+        plt.plot(x,casesActive,label=descriptions[k],color=colors[k],linewidth=3.0*widthScale[k])
 
         plt.text(
-            len(casesActive)-0.5,
+            x[-1]+0.25,
             casesActive[-1],
             descriptions[k],
             horizontalalignment='left',
@@ -58,7 +59,7 @@ for p in range(4):
         plt.gca().set_ylim(bottom=1e-7)
 
     plt.xticks(rotation=75,fontsize=10)
-    plt.gca().set_xlim(left=basedate)
+    plt.gca().set_xlim(left=basedate,right=x[-1])
     plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=mdates.MO))
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     
@@ -73,6 +74,8 @@ for p in range(4):
 
     plt.legend(loc='upper left',fontsize='medium')
 
+    plt.subplots_adjust(right=0.8)
+    
     distutils.dir_util.mkpath('output')
     plt.savefig(
         'output/'+['active-log.png','active-lin.png','active-prop-log.png','active-prop-lin.png'][p],
