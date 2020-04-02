cd1592ec09f058978f8c86ac1ff1c1e13511e976
Tim Day <timday@bottlenose.demon.co.uk>
2020-04-02 10:12:50 +0100

    Another attempt.

--- uk/publish.sh
+++ uk/publish.sh
@@ -3,17 +3,13 @@
 DST=../docs/img/uk
 
 mkdir -p ${DST}
+mkdir -p ${DST}/small
 
 # Use -define png:exclude-chunks=date to stop trivial rebuilds from spamming git
 # see https://imagemagick.org/discourse-server/viewtopic.php?t=21711
 CP="convert -define png:exclude-chunks=date"
 
 for f in cases-log.png cases-aligned-log.png "health-Obesity rate percentage.png" deprivation-Education.png "income-GDHI per head.png" brexit-England.png oldies-England.png oldies-vote-England.png ; do
-    ${CP} "output/${f}" "${DST}/${f}"
+    ${CP}               "output/${f}" "${DST}/${f}"
+    ${CP} -geometry 50% "output/${f}" "${DST}/small/${f}"
 done
-
-mkdir -p ${DST}/small
-
-( cd ${DST} ; for f in *.png ; do convert -geometry 50% -define png:exclude-chunks=date "${f}" "small/${f}" ; done )
-
-( cd ${DST}/small ; for f in *.png ; do mogrify -define png:exclude-chunks=date "${f}" ; done )
