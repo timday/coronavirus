22c3a72ceb29441ed8d02f9ea52893ea5fe6c2da
timday <timday@bottlenose.demon.co.uk>
2020-04-03 00:12:59 +0100

    Notes

--- uk/plot-election.py
+++ uk/plot-election.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Constituency to UTLA from http://geoportal.statistics.gov.uk/datasets/b7435625a4d442bcb331d610f16aacde_0
+
 # Election data from https://commonslibrary.parliament.uk/research-briefings/cbp-8749/ (the accompanying results by constituency csv)
 
-# Constituency to UTLA from http://geoportal.statistics.gov.uk/datasets/b7435625a4d442bcb331d610f16aacde_0
+# Look at votes for con, lab, non-mainstream (including libdem!?), green?, brexit?
