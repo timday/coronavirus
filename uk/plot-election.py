d5fe8e800fc69a04cc4d5f08a7a07a61da42e384
timday <timday@bottlenose.demon.co.uk>
2020-04-03 22:59:38 +0100

    Plot growth rate vs. 2019 election.

--- uk/plot-election.py
+++ uk/plot-election.py
@@ -18,31 +18,17 @@ import scipy.stats
 
 import UKCovid19Data
 
-def getWardToUTLA():
+def cov(x, y, w):
+    return np.sum(w * (x - np.average(x, weights=w)) * (y - np.average(y, weights=w))) / np.sum(w)
 
-    wardToUTLA={}
-    
-    csvfile=open('data/Ward_to_Westminster_Parliamentary_Constituency_to_Local_Authority_District_to_Upper_Tier_Local_Authority_December_2019_Lookup_in_the_United_Kingdom.csv')
-    reader=csv.reader(csvfile)
-    firstRow=True
-    for row in reader:
-        if firstRow:
-            assert row[2]=='PCON19CD'
-            assert row[6]=='UTLA19CD'
-            firstRow=False
-            continue
-
-        ward=row[2]
-        utla=row[6]
-
-        wardToUTLA[ward]=utla
-
-    return wardToUTLA
+def corr(x, y, w):
+    return cov(x, y, w) / np.sqrt(cov(x, x, w) * cov(y, y, w))
 
 def value(x):
     return float(x)
 
-def getVotes(wardToUTLA):
+# Accumulate votes into the areas given (wards and constituencies).  No translation of areas.
+def getRawVotes():
 
     votes={}
 
@@ -63,36 +49,128 @@ def getVotes(wardToUTLA):
             continue
 
         ward=row[0]
-        utla=wardToUTLA[ward]
+        pcon=row[1]
 
-        if not utla in votes:
-            votes[utla]=defaultdict(float)
-            
-        votes[utla]['Total']+=value(row[15])
-        votes[utla]['Con']+=value(row[18])
-        votes[utla]['Lab']+=value(row[19])
-        votes[utla]['LibDem']+=value(row[20])
-        votes[utla]['Brexit']+=value(row[21])
-        votes[utla]['Green']+=value(row[22])
+        for where in set([ward,pcon]):   # Avoid double counting if any duplicates
+
+            if not where in votes:
+                votes[where]=defaultdict(float)
+        
+            votes[where]['Total']+=value(row[15])
+            votes[where]['Con']+=value(row[18])
+            votes[where]['Lab']+=value(row[19])
+            votes[where]['LibDem']+=value(row[20])
+            votes[where]['Brexit']+=value(row[21])
+            votes[where]['Green']+=value(row[22])
 
     return votes
 
-wardToUTLA=getWardToUTLA()
-votes=getVotes(wardToUTLA)
+# For interesting areas, want to collect all their sub-regions
+def getAreas(interesting):
+
+    areas=defaultdict(set)
+    
+    csvfile=open('data/Ward_to_Westminster_Parliamentary_Constituency_to_Local_Authority_District_to_Upper_Tier_Local_Authority_December_2019_Lookup_in_the_United_Kingdom.csv')
+    reader=csv.reader(csvfile)
+    firstRow=True
+    for row in reader:
+        if firstRow:
+            assert row[0][-6:]=='WD19CD'
+            assert row[2]=='PCON19CD'
+            assert row[4]=='LAD19CD'
+            assert row[6]=='UTLA19CD'
+            firstRow=False
+            continue
+
+        ward=row[0]
+        pcon=row[2]
+        lad=row[4]
+        utla=row[6]
+
+        if utla in interesting:
+            areas[utla].add(utla)
+            areas[utla].add(lad)
+            areas[utla].add(pcon)
+            areas[utla].add(ward)
+
+        if lad in interesting:
+            areas[lad].add(lad)
+            areas[lad].add(pcon)
+            areas[lad].add(ward)
+
+        if pcon in interesting:
+            areas[pcon].add(pcon)
+            areas[pcon].add(ward)
+
+        if ward in interesting:
+            areas[ward].add(ward)
+
+    return dict(areas)
+
 
 window=7
 
 timeseries,dates,codes=UKCovid19Data.getUKCovid19Data('England',window+1,None)
 
-fig=plt.figure(figsize=(8,6))
-
 rate={k:(timeseries[k][-1]/timeseries[k][-1-window])**(1.0/(window))-1.0 for k in timeseries.keys() if timeseries[k][-1-window]>0.0}
+interesting=frozenset(rate.keys())
 
-x=[100.0*(rate[k]-1.0) for k in rate.keys()]
-y=[100.0*votes[k]['Con']/votes[k]['Total'] for k in rate.keys()]
-w=[votes[k]['Total'] for k in rate.keys()]
-s=np.sqrt(w/1000.0)
+rawvotes=getRawVotes()
+areas=getAreas(interesting)
 
-plt.scatter(x,y,s=s)
+votes={}
 
+for c in interesting:
+    
+    if not c in votes:
+        votes[c]=defaultdict(float)
+
+    for a in areas[c]:
+        if a in rawvotes:
+            for k in rawvotes[a]:
+                votes[c][k]+=rawvotes[a][k]
+            
+print len(interesting),'interesting'
+novotes=[k for k in interesting if votes[k]['Total']==0.0]
+print 'No votes for',len(novotes),':',novotes
+
+for party in ['Con','Lab','LibDem','Brexit','Green']:
+
+    fig=plt.figure(figsize=(8,6))
+
+    print 'Top 5 for',party,sorted([(100.0*votes[k][party]/votes[k]['Total'],k) for k in rate.keys()],key=lambda it: it[0],reverse=True)[:5]
+    
+    y=np.array([100.0*rate[k] for k in rate.keys()])
+    x=np.array([100.0*votes[k][party]/votes[k]['Total'] for k in rate.keys()])
+    w=np.array([votes[k]['Total'] for k in rate.keys()])
+    s=np.sqrt(w/100.0)
+    
+    plt.scatter(x,y,s=s)
+
+    # Unweighted regression line
+    r=scipy.stats.linregress(x,y)
+    gradient,intercept,r_value,p_value,std_err=r
+    print 'Unweighted',gradient,intercept
+    
+    rx=np.linspace(min(x),max(x),100)
+    ry=gradient*rx+intercept
+    plt.plot(rx,ry,color='tab:orange',label='Linear regression (unweighted)')
+    
+    # Weighted regression line
+    coef=np.polyfit(x,y,1,w=w)
+    print 'Weighted',coef[0],coef[1]
+    ry=coef[1]+coef[0]*rx  # Highest power first
+    plt.plot(rx,ry,color='tab:red',label='Linear regression (weighted by total votes)')
+    rw=corr(x,y,w)
+    
+    coef=np.polyfit(x,y,2,w=w)
+    qy=coef[2]+coef[1]*rx+coef[0]*rx**2
+    plt.plot(rx,qy,color='tab:green',label='Quadratic best fit (weighted)')
+
+    plt.title('Case-count growth rate vs. {} vote share'.format(party))
+    
 plt.show()
+
+# So covid case data has E06000036 in it.
+# Ward data has e.g E05002273,E14000586,E06000036 under it.
+# Votes data has the second one.
