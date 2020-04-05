d7630cd75a64f286645d6181677ba69ef7a5ed28
timday <timday@bottlenose.demon.co.uk>
2020-04-05 12:51:38 +0100

    Will add proportion of population plot.

--- global/publish.sh
+++ global/publish.sh
@@ -9,7 +9,7 @@ mkdir -p ${DST}/small
 # see https://imagemagick.org/discourse-server/viewtopic.php?t=21711
 CP="convert -define png:exclude-chunks=date"
 
-for f in growth.png aligned-cases.png aligned-deaths.png active-log.png projections-0.png projections-1.png projections-2.png projections-3.png ; do
+for f in growth.png aligned-cases.png aligned-deaths.png active-log.png active-prop-log.png projections-0.png projections-1.png projections-2.png projections-3.png ; do
     ${CP}               "output/${f}" "${DST}/${f}"
     ${CP} -geometry 50% "output/${f}" "${DST}/small/${f}"
 done
