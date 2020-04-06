020213d14be39df7c71a039e063337f2a70888d7
timday <timday@bottlenose.demon.co.uk>
2020-04-06 23:43:10 +0100

    Interesting but not useful.

--- experiment/experiment8.py
+++ experiment/experiment8.py
@@ -10,45 +10,52 @@ import matplotlib.pyplot as plt
 # Growth rate proportional to active cases.
 # Maybe with herd immunity or suppression factor too but keep it simple. 
 
-# This is the initially infectious source
-def impulse(t,d):
-    if t<=0.0:
-        return 0.0
-    else:
-        return 10.0*math.exp(-d*t)
-
-def values_before_zero0(t):
-    return np.array([0.0,0.0])
-
+x0=10.0
 k=0.2
-T=14.0
-d=0.1
-P=1e7
-a=0.01
+T0=14.0  # Active window
+
+T1=21.0   
+P=1e8
+a=0.1
+b=0.01
+
+def values_before_zero(t):
+    v=x0*math.exp(k*t)
+    return np.array([v,0.0])
 
 # Needs to return derivatives
-# Need two channels 'cos something fails in ddeint otherwise
+# Second channel is policy response
 def model0(Y,t):
 
-    ynow=Y(t)[0]
-    ythen=Y(t-T)[0]
+    ynow=Y(t)
 
-    active=ynow-ythen+impulse(t,d)
+    active=ynow[0]-Y(t-T0)[0]
 
-    rate=(k*active/(1.0+a*t))*(P-ynow)/P
+    pactive1=Y(t-T1)[0]-Y(t-T1-T0)[0]
+    pactive0=Y(t-T1-7.0)[0]-Y(t-T1-T0-7.0)[0]
 
-    print t,ynow,ythen,rate
+    g=max(0.0,(pactive1/pactive0)**(1.0/7.0)-1.0)  # Growth rate in active cases over week to T1 ago
     
-    return np.array([rate,0.0])
+    dcases=(k*active*np.exp(-ynow[1]))*(1.0-ynow[0]/P)
+    dpolicy=a*g-b*ynow[1]
 
-ts=np.linspace(0.0,180.0,1800)
-ys=ddeint(model0,values_before_zero0,ts)[:,0]
+    return np.array([dcases,dpolicy])
+
+ts=np.linspace(0.0,210.0,2401)
+ys=ddeint(model0,values_before_zero,ts)
 
 # Considered multiple channels for various periods of incubation... but seems to get monstrously complicated quickly.
 
-plt.plot(ts,ys,color='blue')
-plt.plot(ts,ys-np.concatenate([np.zeros((14,)),ys[:-14]]),color='red')
+fig=plt.figure(figsize=(15,6))
 
+plt.subplot(1,2,1)
+plt.plot(ts,ys[:,0],color='blue')
+plt.plot(ts[140:],ys[140:,0]-ys[:-140,0],color='red')
 plt.yscale('symlog')
 plt.grid(True)
+
+plt.subplot(1,2,2)
+plt.plot(ts,ys[:,1])
+plt.grid(True)
+
 plt.show()
