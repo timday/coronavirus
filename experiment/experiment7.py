f2c9ddd36ecf3935cd5fd81ab86511d24037b866
Tim Day <timday@bottlenose.demon.co.uk>
2020-04-06 15:33:15 +0100

    Some sort of model.

--- experiment/experiment7.py
+++ experiment/experiment7.py
@@ -5,63 +5,50 @@ import numpy as np
 from ddeint import ddeint
 import matplotlib.pyplot as plt
 
-Ti=14.0
-Tc=7.0
+# Revisit DDE models.
+# Want an active cases number: cases now - cases T weeks ago.
+# Growth rate proportional to active cases.
+# Maybe with herd immunity or suppression factor too but keep it simple. 
 
-def impulse(t):
+
+# This is the initially infectious source
+def impulse(t,d):
     if t<=0.0:
         return 0.0
     else:
-        return 10.0*math.exp(-t)
-
-# Elements:
-#  0: Number incubating
-#  1: Number contagious
-#  2: Number observed
+        return 10.0*math.exp(-d*t)
 
 def values_before_zero0(t):
-    return np.array([0.0,0.0,0.0])
+    return np.array([0.0,0.0])
+
+k=0.2
+T=14.0
+d=0.1
+P=1e7
+a=0.01
 
 # Needs to return derivatives
+# Need two channels 'cos something fails in ddeint otherwise
 def model0(Y,t):
 
-    k=1.25
+    ynow=Y(t)[0]
+    ythen=Y(t-T)[0]
+
+    active=ynow-ythen+impulse(t,d)
+
+    rate=(k*active/(1.0+a*t))*(P-ynow)/P
+
+    print t,ynow,ythen,rate
     
-    y=Y(t)
+    return np.array([rate,0.0])
 
-    print t,y
-
-    yp=Y(t-Ti)
-    ypp=Y(t-Ti-Tc)
-    yppp=Y(t-Ti-0.5*Tc)
-
-    i=impulse(t)
-    ip=impulse(t-Ti)
-    ipp=impulse(t-Ti-Tc)
-    ippp=impulse(t-Ti-0.5*Tc)
-    
-    i_now=k*y[1]+i
-    i_then=k*yp[1]+ip
-    
-    c_now=i_then
-    c_then=k*ypp[1]+ipp
-
-    m=k*yppp[1]+ippp
-    
-    return np.array([
-        i_now-i_then, # Incubating
-        c_now-c_then, # Contagious
-        0.1*m         # Observed
-    ])
-
-ts=np.linspace(0.0,90.0,901)
-y0s=ddeint(model0,values_before_zero0,ts)
+ts=np.linspace(0.0,180.0,1800)
+ys=ddeint(model0,values_before_zero0,ts)[:,0]
 
 # Considered multiple channels for various periods of incubation... but seems to get monstrously complicated quickly.
 
-plt.plot(ts,y0s[:,0],color='blue')
-plt.plot(ts,y0s[:,1],color='green')
-plt.plot(ts,y0s[:,2],color='red')
+plt.plot(ts,ys,color='blue')
+plt.plot(ts,ys-np.concatenate([np.zeros((14,)),ys[:-14]]),color='red')
 
 plt.yscale('symlog')
 plt.grid(True)
