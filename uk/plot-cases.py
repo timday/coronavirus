824ea1354948f9fc85622c787304ecae8da980f7
Tim Day <timday@bottlenose.demon.co.uk>
2020-04-24 15:07:00 +0100

    More UK plots: active cases and all plots linear and log scale.

--- uk/plot-cases.py
+++ uk/plot-cases.py
@@ -13,6 +13,16 @@ import UKCovid19Data
 
 extrapolationWindow=7
 
+activeWindowLo=14
+activeWindowHi=21
+
+def sweep(cases,window):
+    return cases-np.concatenate([np.zeros((window,)),cases[:-window]])
+
+def active(casesTotal):
+    casesActive=sum([sweep(casesTotal,w) for w in xrange(activeWindowLo,activeWindowHi+1)])/(1+activeWindowHi-activeWindowLo)
+    return casesActive
+
 # Compute shift in days to align data on common
 def computeWhen(data,common):
     if data[-1]<common:
@@ -42,7 +52,7 @@ def computeWhen(data,common):
 
 common=30.0
 
-for chart in [0,1,2]:
+for chart in [0,1,2,3,4,5]:  # 0,1 cases, 2,3 cases aligned 4,5 active cases
 
     fig=plt.figure(figsize=(16,9))
     
@@ -63,16 +73,19 @@ for chart in [0,1,2]:
         for k in sorted(timeseries.keys(),key=lambda k: timeseries[k][-1],reverse=False):   # Plot highest current case counts with higher z
             cases=np.array([y for y in timeseries[k]])
 
+            if chart==4 or chart==5:
+                cases=active(cases)
+            
             assert len(days)==len(cases)
 
-            if chart==2:
+            if chart==2 or chart==3:
                 base=computeWhen(cases,common)
                 if what[0]=='Wales':
                     base+=14.0  # Starts 14 days after England and Scotland
             else:
                 base=0.0
             
-            if chart>=1:
+            if chart%2==1:
                 cases[cases<10.0]=np.nan
 
             region=UKCovid19Data.whichRegion(k)
@@ -98,7 +111,7 @@ for chart in [0,1,2]:
     
     for txt in texts:
         msg=txt[1]
-        if chart==2 and txt[2]!=0.0:
+        if (chart==2 or chart==3) and txt[2]!=0.0:
             msg=msg+(' ({:+.1f} days)'.format(-(txt[2]-averagebase)))
             print '{:6d} '.format(int(txt[0]))+msg
         plt.text(mdayshi+0.1,txt[0],msg,horizontalalignment='left',verticalalignment='center',fontdict={'size':8,'alpha':0.8,'weight':'bold','color':txt[3]},zorder=txt[4]) 
@@ -108,7 +121,7 @@ for chart in [0,1,2]:
             
     plt.subplots_adjust(right=0.8)
 
-    if chart==2:
+    if chart==2 or chart==3:
         def relativedate(x,pos=None):
             return '{:+.0f}'.format(x-mdates.date2num(datetime.date(2020,3,7)))
         
@@ -119,27 +132,36 @@ for chart in [0,1,2]:
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     plt.xticks(rotation=75,fontsize=10)
         
-    if chart==2:
+    if chart==2 or chart==3:
         mdayslo+=14 # Fudge, as most of it's not interesting.  May need edit if adjust skip parameter.
     plt.xlim(left=mdayslo,right=mdayshi)
-    plt.ylim(bottom={0:0.0,1:10.0,2:10.0}[chart])
-    plt.gca().set_ylabel('Cumulative cases')
-    if chart>=1:
+    if chart%2==1:
         plt.yscale('symlog')
-
-    if chart==0:
-        plt.title('Cases by UTLA.  Data to {}.'.format(days[-1]))
-    elif chart==1:
-        plt.title('Cases by UTLA (log scale) from $\geq10$  Data to {}.'.format(days[-1]))
+        plt.ylim(bottom=10.0)
     else:
-        plt.title('Cases by UTLA (log scale) from $\geq10$ aligned on {} cases\nTimes +/- ahead/behind average  Data to {}.'.format(common,days[-1]))
+        plt.ylim(bottom=0.0)        
+
+    if chart<=3:
+        plt.gca().set_ylabel('Cumulative cases')
+    else:
+        plt.gca().set_ylabel('Active cases')
+
+    if chart==0 or chart==1:
+        plt.title('Cases by UTLA.  Data to {}.'.format(days[-1]))
+    elif chart==2 or chart==3:
+        plt.title('Cases by UTLA aligned on {} cases\nTimes +/- ahead/behind average.  Data to {}.'.format(common,days[-1]))
+    else:        
+        plt.title('Active cases by UTLA.  {}-{} days active duration.  Data to {}.'.format(activeWindowLo,activeWindowHi,days[-1]))
 
     distutils.dir_util.mkpath('output')
     plt.savefig(
         {
             0:'output/cases.png',
             1:'output/cases-log.png',
-            2:'output/cases-aligned-log.png'
+            2:'output/cases-aligned.png',
+            3:'output/cases-aligned-log.png',
+            4:'output/cases-active.png',
+            5:'output/cases-active-log.png'
         }[chart]
         ,dpi=96
     )
