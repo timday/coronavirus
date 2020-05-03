641089685c3d1cbf365e5625ed6c9b6bf43e1aa0
Tim Day <timday@bottlenose.demon.co.uk>
2020-05-03 10:09:21 +0100

    New data

--- uk/doit.sh
+++ uk/doit.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 ./plot-cases.py &
-sleep 5
-./plot-deprivation.py &
-sleep 5
+sleep 10
 ./plot-health.py &
-sleep 5
-./plot-brexit.py &
-sleep 5
-./plot-election.py &
-sleep 5
+sleep 10
+./plot-deprivation.py &
+sleep 10
 ./plot-income.py &
+sleep 10
+./plot-brexit.py &
+sleep 10
+./plot-election.py &
 
 # ./plot-growth.py  # Just prints stuff currently.
